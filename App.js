5f7acaf611b144a8f3284c6ad0d2752d319e7570
rohitt05 <rohitkattimani172@gmail.com>
2023-03-24 14:48:17 +0530

    init & Home Screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/screens/Home";
+
+export default function App() {
+  return (
+   <View>
+    <HomeScreen/>
+   </View>
+  );
+}
+
+
